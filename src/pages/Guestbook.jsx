e4c5dbbe18ff37190a88d8ce24e37153234828a8
Marco Romero <marcod.romero@gmail.com>
2025-08-23 02:42:39 -0300

    Agregar libro de visitas

--- src/pages/Guestbook.jsx
+++ src/pages/Guestbook.jsx
@@ -11,7 +11,11 @@ export default function Guestbook() {
         <h1 className="text-[18px] text-white text-right w-[500px]">Libro de visitas</h1>
       </div>
       <section>
-        
+          <iframe 
+            src="https://guestbooks.meadow.cafe/guestbook/857" 
+            className=" w-full h-full min-h-[530px] md:min-h-[340px] md:h-[340px] border-ridge"
+            >
+          </iframe>
       </section>
        <div className="h-[60px] flex items-center justify-end">
         <NavLink to="/"><img src={btnReturn} className="zoom  w-[40px] h-[40px]"/></NavLink>
